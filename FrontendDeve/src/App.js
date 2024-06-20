a4968367a4bc7061270c0820dfc2bd9008c71750
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-20 23:42:03 +0530

    Late-night-push

--- FrontendDeve/src/App.js
+++ FrontendDeve/src/App.js
@@ -3,12 +3,13 @@ import NavBar from "./components/navbar/Nav.jsx";
 import Main from "./components/Cards/Main";
 import Cards from "./components/Cards/Cards";
 import Card from "./components/Cards/Cards2";
-
+import Logo from "./components/Logo.js";
 import ComponenetA from "./Testing/ComponentA";
 
 const App = () => {
   return (
     <>
+    <Logo/>
 <ComponenetA/>
     <NavBar />
       <Main />
---
+++ FrontendDeve/src/components/Logo.js
@@ -0,0 +1,14 @@
+import react from "react"
+import reactLogo from "../assets/KfcAnime.jpg"
+const Logo = ()=>{
+    return (
+
+    <>
+    <div>
+        <img src={reactLogo} alt="img" style={{ width: '100px', height: 'auto' }} />
+    </div>
+
+
+    </>)
+}
+export default Logo;
