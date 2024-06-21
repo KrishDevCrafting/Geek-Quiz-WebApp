7f6e212470d32096e35c892b64be8dcd6f50dddb
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-21 23:13:13 +0530

    Updated

--- FrontendDeve/src/components/Cards/Cards2.jsx
+++ FrontendDeve/src/components/Cards/Cards2.jsx
@@ -28,21 +28,21 @@ const Card = () => {
         {/* End-card-1 */}
         {/* Card-2 */}
         <section>
-          <div className="main-1">
-            <div class="card card-1">
-              <div class="border"></div>
-              <div class="content">
-                <div class="logo">
+          <div className="main-container-2">
+            <div class="box-card-2 card-2">
+              <div class="border-2"></div>
+              <div class="content-2">
+                <div class="card-logo-2">
                   <div class="logo2">
-                    <div className="logo3">
+                    <div className="logo2">
                       <h1>Empty-Space</h1>
                     </div>
                   </div>
-                  <span class="trail"></span>
+                  <span class="trail-2"></span>
                 </div>
-                <span class="logo-bottom-text">Krish.io</span>
+                <span class="logo-bottom-text-2">Krish.io</span>
               </div>
-              <span class="bottom-text">Space.io</span>
+              <span class="bottom-text-2">Space.io</span>
             </div>
           </div>
         </section>
