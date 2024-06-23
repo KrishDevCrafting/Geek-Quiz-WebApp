4b040a375264b9aa5da14a034f9a581cc512c06b
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-24 00:03:18 +0530

    Frontend

--- FrontendDeve/src/components/Cards/Cards2.jsx
+++ FrontendDeve/src/components/Cards/Cards2.jsx
@@ -3,11 +3,13 @@ import "./Card2.css";
 const Card = () => {
   return (
     <>
+    <div><h1 id="Card-2-Heading">CAR-QUIZ</h1></div>
+      
       <div className="main-card">
         {/* card-1   */}
 
         <section>
-     <h1 style={{border: "1px soild red"}}>CAR-QUIZ</h1>     
+        
           <div className="main-container-2">
             <div class="box-card card-2">
               <div class="border-2"></div>
