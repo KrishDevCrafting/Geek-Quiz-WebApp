c66d6b3a50de1f5bd8c59ac93dcc1916491943c3
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-30 23:54:25 +0530

    Frontend

--- FrontendDeve/src/components/Pages/Auth/Login.jsx
+++ FrontendDeve/src/components/Pages/Auth/Login.jsx
@@ -13,9 +13,9 @@ export const LoginComponent = () => {
   
   
   
-  
-  
-  
+  {/* https://www.figma.com/design/xwArmcwnm03l7PlheeTF1P/Login_Sign-up-Templates-(Desktop)-(Community)?node-id=12-1550&t=Wb2YjFdouOiLwN6l-0 */}
+{/*   
+  https://www.figma.com/design/6oGdfW3UgywyvxsPUhtdqd/100-Day-Challenge?node-id=6-3&t=4A6HVs10WUtZ3bQQ-0 */}
   
   </>;
 };
