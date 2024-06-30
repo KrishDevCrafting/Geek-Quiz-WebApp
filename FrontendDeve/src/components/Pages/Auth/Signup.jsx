bc753efc2203b1dd8f6fdf8e2924a95489b4a6ee
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-30 22:59:28 +0530

    Frontend

--- FrontendDeve/src/components/Pages/Auth/Signup.jsx
+++ FrontendDeve/src/components/Pages/Auth/Signup.jsx
@@ -7,20 +7,24 @@ const Signup = () => {
           <h1>Sign-up</h1>
           <div>
             <span>
-              Name: <input type="text" />
+              Name: <input type="text" placeholder="UserName" />
             </span>
           </div>
 
           <div>
             <span>
-              Email: <input type="text" name="" id="" />
+              Email: <input type="email" name="" id="" placeholder="Email" required/>
             </span>
           </div>
 
           <div>
             <span>
-              Password: <input type="number" name="" id="" />
+              Password:{" "}
+              <input type="password" name="" id="" placeholder="Password" />
             </span>
+          </div>
+          <div>
+            <button>Submit-here</button>
           </div>
         </div>
       </div>
