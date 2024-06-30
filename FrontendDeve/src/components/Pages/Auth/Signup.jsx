e8c2f75925f7eeb88d835473a211b0b9a80d7716
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-30 22:55:17 +0530

    Frontend

--- FrontendDeve/src/components/Pages/Auth/Signup.jsx
+++ FrontendDeve/src/components/Pages/Auth/Signup.jsx
@@ -2,28 +2,28 @@ import React from "react";
 const Signup = () => {
   return (
     <>
+      <div className="container-box">
+        <div className="box">
+          <h1>Sign-up</h1>
+          <div>
+            <span>
+              Name: <input type="text" />
+            </span>
+          </div>
 
+          <div>
+            <span>
+              Email: <input type="text" name="" id="" />
+            </span>
+          </div>
 
-
-
-<div className="container-box">
-<div className="box">
- 
-</div>
-
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
+          <div>
+            <span>
+              Password: <input type="number" name="" id="" />
+            </span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
