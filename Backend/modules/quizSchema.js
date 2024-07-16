0ec7ddc0a2ba2dde22852fd8c3f070341b8d003e
KrishDevCrafting <krishkumer150@gmail.com>
2024-07-16 23:36:49 +0530

    Backend-api

--- Backend/modules/quizSchema.js
+++ Backend/modules/quizSchema.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
 const questionScheme = new mongoose.Schema({
+  text: { type: String, required: true },
+  iscorrect: { type: Boolean, required: true },
+});
 
+const answerSchema = new mongoose.Schema({
+  questionText: { type: String, required: true },
+  answers: [answerSchema],
+});
 
-
-
-
-    
+const quizSchma = new mongoose.String({
+  title: { type: String, required: true },
+  category: { type: String, required: true },
+  question: { questionScheme },
 });
