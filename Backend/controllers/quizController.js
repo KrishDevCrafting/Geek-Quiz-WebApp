a3e069c92d320f5235314ff65b2df709f33bd4cf
KrishDevCrafting <krishkumer150@gmail.com>
2024-07-17 23:27:59 +0530

    Backend-api

--- Backend/controllers/quizController.js
+++ Backend/controllers/quizController.js
@@ -1,11 +1,11 @@
 const Quiz = require("../models/quizSchema");
 
 const creatQuiz = async (req, res) => {
-    try {
-        const quiz = new Quiz(req.body);
-        await quiz.save();
-        res.status(201).send(quiz);
-      } catch (error) {
-        res.status(400).send(error);
-      }
+  try {
+    const quiz = new Quiz(req.body);
+    await quiz.save();
+    res.status(201).send(quiz);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
