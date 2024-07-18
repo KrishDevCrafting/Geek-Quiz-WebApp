651d2576788377752b00ed5fe769c6d4dcc6a1a4
KrishDevCrafting <krishkumer150@gmail.com>
2024-07-18 12:14:51 +0530

    never-giveUp-Krish

--- Backend/controllers/quizController.js
+++ Backend/controllers/quizController.js
@@ -9,3 +9,28 @@ const creatQuiz = async (req, res) => {
     res.status(400).send(error);
   }
 };
+
+// get all quizzes
+
+const getAllQuizzes = async () => {
+  try {
+    const quizzes = await Quiz.find({});
+    res.status(200).send(quizzes);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+// get a single Quiz by ID
+
+const getQuizById = async (req, res) => {
+  try {
+    const quiz = await Quiz.findById(req.params.id);
+    if (!quiz) {
+      return res.status(404).send();
+    }
+    res.status(200).send(quiz);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
