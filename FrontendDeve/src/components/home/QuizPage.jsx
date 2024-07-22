6394fd302558006bd3e93ce908a121295a65faec
Krish <Krishkumer150@gamil.com>
2024-07-22 15:06:43 +0530

    Frontend

--- FrontendDeve/src/components/home/QuizPage.jsx
+++ FrontendDeve/src/components/home/QuizPage.jsx
@@ -3,8 +3,35 @@ import React from "react";
 export const QuizComponent = () => {
   return (
     <>
-      <div>
-        <h1 className="text-rose-700 text-9xl">Quiz-Page</h1>
+      <div
+        className="flex justify-center  items-center"
+        style={{ border: "1px solid red", height: "50vh" }}>
+        <form action="">
+          <label>
+            <h1>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+              ea.
+            </h1>
+          </label>
+          <section className="text-center">
+            <div>
+              <h3 className="inline-block">option-1</h3>
+              <input type="checkbox" name="" id="" />
+            </div>
+            <div>
+              <h3 className="inline-block">option-2</h3>
+              <input type="checkbox" name="" id="" />
+            </div>
+            <div>
+              <h3 className="inline-block">option-3</h3>
+              <input type="checkbox" name="" id="" />
+            </div>
+            <div>
+              <h3 className="inline-block">option-4</h3>
+              <input type="checkbox" name="" id="" />
+            </div>
+          </section>
+        </form>
       </div>
     </>
   );
