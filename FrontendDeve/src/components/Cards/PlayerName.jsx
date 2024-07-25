dde1b68681ba6779a9064c72d050400ba4c3ec5b
KrishDevCrafting <krishkumer150@gmail.com>
2024-07-25 22:31:18 +0530

    commo-changes

--- FrontendDeve/src/components/Cards/PlayerName.jsx
+++ FrontendDeve/src/components/Cards/PlayerName.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../Cards/PlayerName.css";
 const DynamicName = () => {
+  const [data, setdata] = React.useState([]);
+  const holdData = "http://localhost:7000/user/get";
+
+  useEffect(() => {
+    const dynamicData = async () => {
+      const response = await fetch(holdData);
+      const res = await response.json();
+      setdata(res);
+    };
+    dynamicData();
+  }, []);
+
   return (
     <>
       {/* section 2 started here */}
-      <section>
-        <div className="section2">
-          <h1 id="text-align">
-            <ul>
-              <li>Top Players - 🎮</li>
-              <li>Jeo</li>
-              <li>Sam</li>
-              <li>siddhart Damm</li>
-            </ul>
-          </h1>
+      <section className="section2">
+        <div className="">
+          <h1 id="">Top player 🎮</h1>
+          <ul>
+            {data.map((e) => (
+              <li>{e.title}</li>
+            ))}
+          </ul>
         </div>
       </section>
     </>
