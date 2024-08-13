c39b6ef3b3a8c1dcd223fba5c113977ade1bf469
KrishDevCrafting <krishkumer150@gmail.com>
2024-08-13 17:40:39 +0530

    chanegs

---
+++ FrontendDeve/src/components/Features/QLogic.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initiStateValue = "";
+const QuizLogic = createSlice({
+  name: "cardsLogic",
+  initialState: {
+    value: initiStateValue,
+  },
+  reducers: {
+    DynamicQuiz: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+export const { DynamicQuiz } = QuizLogic.actions;
+export default QuizLogic.reducer;
