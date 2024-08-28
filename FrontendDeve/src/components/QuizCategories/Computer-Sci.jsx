9b81bb600f7153f94288b123f4d865988259355d
Krish <krishkumer150@gmail.com>
2024-08-28 22:03:24 +0530

    Frontend

--- FrontendDeve/src/components/QuizCategories/Computer-Sci.jsx
+++ FrontendDeve/src/components/QuizCategories/Computer-Sci.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import bgImage from "../../assets/DATA-STRUCTURE-ALGORITHUM.gif";
+import computerNetworkImg from "../../assets/Computer-network.gif";
+import dbmsImage from "../../assets/dbms.gif";
+import "./cs.css"
 const PageCs = () => {
   return (
     <>
@@ -8,23 +12,80 @@ const PageCs = () => {
     .cs{
     color: red;
     }
+
+    .box-card {
+    background-image: url(${bgImage}) !important;
+    }
+    .box-card-two{
+       background-image: url(${computerNetworkImg}) !important;
+    }
+       .box-card-three{
+       background-image: url(${dbmsImage})} !important;
+       
+          }
       `}
         </style>
       </div>
-      <div>
+      <div className="parent-container">
         <nav>
           <h1 className="cs">Computer-science-page!</h1>
         </nav>
+        {/* section-1 */}
 
-        <section>
+        <div className="main-card">
+          <section>
+            <div className="card">
+              <div className="box-card card-2 w-3 bg-hero-pattern">
+                <div className="border-2"></div>
+                <div className="content-2">
+                  <div className="card-logo-2">
+                    <div className="logo2">
+                      <div className="logo2">
+                        <span>DSA</span>
+                      </div>
+                    </div>
+                    <span className="trail-2"></span>
+                  </div>
+                  <span className="logo-bottom-text-2">Krish.io</span>
+                </div>
+                <span className="bottom-text-2">Space.io</span>
+              </div>
+            </div>
+          </section>
+          {/* section-end */}
+
+          {/* box-2 started */}
+          <section>
+            <div className="card">
+              <div className="box-card-two card-2 w-3">
+                <div className="border-2"></div>
+                <div className="content-2">
+                  <div className="card-logo-2">
+                    <div className="logo2">
+                      <div className="logo2">
+                        <span>Computer-Networking</span>
+                      </div>
+                    </div>
+                    <span className="trail-2"></span>
+                  </div>
+                  <span className="logo-bottom-text-2">Krish.io</span>
+                </div>
+                <span className="bottom-text-2">Space.io</span>
+              </div>
+            </div>
+          </section>
+          {/* box-2 End */}
+
+          {/* box-3 started */}
+
           <div className="card">
-            <div className="box-card card-2 w-3 bg-hero-pattern">
+            <div className="box-card-three card-2 w-3">
               <div className="border-2"></div>
               <div className="content-2">
                 <div className="card-logo-2">
                   <div className="logo2">
                     <div className="logo2">
-                      <span>DSA</span>
+                      <span>Database-Management-System</span>
                     </div>
                   </div>
                   <span className="trail-2"></span>
@@ -34,7 +95,8 @@ const PageCs = () => {
               <span className="bottom-text-2">Space.io</span>
             </div>
           </div>
-        </section>
+          {/* box-3 Ended */}
+        </div>
       </div>
     </>
   );
