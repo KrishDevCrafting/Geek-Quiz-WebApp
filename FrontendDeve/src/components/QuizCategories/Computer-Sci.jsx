5900c1e327c164a26b6cb299ff26197d0a607709
Krish <krishkumer150@gmail.com>
2024-08-29 15:24:33 +0530

    Frontend

--- FrontendDeve/src/components/QuizCategories/Computer-Sci.jsx
+++ FrontendDeve/src/components/QuizCategories/Computer-Sci.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import bgImage from "../../assets/DATA-STRUCTURE-ALGORITHUM.gif";
 import computerNetworkImg from "../../assets/Computer-network.gif";
 import dbmsImage from "../../assets/dbms.gif";
-import "./cs.css"
+import "./cs.css";
 const PageCs = () => {
   return (
     <>
       <div>
         <style>
           {`
-    .cs{
-    color: red;
-    }
+    
 
     .box-card {
     background-image: url(${bgImage}) !important;
@@ -28,10 +26,11 @@ const PageCs = () => {
       </div>
       <div className="parent-container">
         <nav>
-          <h1 className="cs">Computer-science-page!</h1>
+          <h1 className="text-white text-4xl text-center">
+            Computer-science-page!
+          </h1>
         </nav>
         {/* section-1 */}
-
         <div className="main-card">
           <section>
             <div className="card">
@@ -97,7 +96,124 @@ const PageCs = () => {
           </div>
           {/* box-3 Ended */}
         </div>
+        {/* Section-2 covering all cards */}
+        <section className="flex justify-around">
+          <div className="card">
+            <div className="box-card card-2 w-3 bg-hero-pattern">
+              <div className="border-2"></div>
+              <div className="content-2">
+                <div className="card-logo-2">
+                  <div className="logo2">
+                    <span>DSA</span>
+                  </div>
+                  <span className="trail-2"></span>
+                </div>
+                <span className="logo-bottom-text-2">Krish.io</span>
+                {/* Add a unique identifier */}
+              </div>
+              <span className="bottom-text-2">Space.io</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="box-card card-2 w-3 bg-hero-pattern">
+              <div className="border-2"></div>
+              <div className="content-2">
+                <div className="card-logo-2">
+                  <div className="logo2">
+                    <span>DSA</span>
+                  </div>
+                  <span className="trail-2"></span>
+                </div>
+                <span className="logo-bottom-text-2">Krish.io</span>
+                {/* Add a unique identifier */}
+              </div>
+              <span className="bottom-text-2">Space.io</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="box-card card-2 w-3 bg-hero-pattern">
+              <div className="border-2"></div>
+              <div className="content-2">
+                <div className="card-logo-2">
+                  <div className="logo2">
+                    <span>DSA</span>
+                  </div>
+                  <span className="trail-2"></span>
+                </div>
+                <span className="logo-bottom-text-2">Krish.io</span>
+                {/* Add a unique identifier */}
+              </div>
+              <span className="bottom-text-2">Space.io</span>
+            </div>
+          </div>
+        </section>
+        {/* End of Section covering all cards */}
+
+        {/* Section-3 */}
+
+        <section className="flex justify-around">
+          <div className="card">
+            <div className="box-card card-2 w-3 bg-hero-pattern">
+              <div className="border-2"></div>
+              <div className="content-2">
+                <div className="card-logo-2">
+                  <div className="logo2">
+                    <span>DSA</span>
+                  </div>
+                  <span className="trail-2"></span>
+                </div>
+                <span className="logo-bottom-text-2">Krish.io</span>
+                {/* Add a unique identifier */}
+              </div>
+              <span className="bottom-text-2">Space.io</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="box-card card-2 w-3 bg-hero-pattern">
+              <div className="border-2"></div>
+              <div className="content-2">
+                <div className="card-logo-2">
+                  <div className="logo2">
+                    <span>DSA</span>
+                  </div>
+                  <span className="trail-2"></span>
+                </div>
+                <span className="logo-bottom-text-2">Krish.io</span>
+                {/* Add a unique identifier */}
+              </div>
+              <span className="bottom-text-2">Space.io</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="box-card card-2 w-3 bg-hero-pattern">
+              <div className="border-2"></div>
+              <div className="content-2">
+                <div className="card-logo-2">
+                  <div className="logo2">
+                    <span>DSA</span>
+                  </div>
+                  <span className="trail-2"></span>
+                </div>
+                <span className="logo-bottom-text-2">Krish.io</span>
+                {/* Add a unique identifier */}
+              </div>
+              <span className="bottom-text-2">Space.io</span>
+            </div>
+          </div>
+        </section>
+        {/* Section-3 End! */}
+
+
+
+{/* Section-4 Started */}
+{/* Section */}
+
       </div>
+      {/* 👆🏻 Main div for whole component! */}
     </>
   );
 };
