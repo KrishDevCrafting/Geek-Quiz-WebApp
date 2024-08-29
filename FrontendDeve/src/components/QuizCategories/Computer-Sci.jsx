de27815f13eb6ad4b88d4fc92d1a8617c2b7d70a
Krish <krishkumer150@gmail.com>
2024-08-29 16:39:32 +0530

    Frontend

--- FrontendDeve/src/components/QuizCategories/Computer-Sci.jsx
+++ FrontendDeve/src/components/QuizCategories/Computer-Sci.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import bgImage from "../../assets/DATA-STRUCTURE-ALGORITHUM.gif";
 import computerNetworkImg from "../../assets/Computer-network.gif";
 import dbmsImage from "../../assets/dbms.gif";
+import cloudComputingImage from "../../assets/cloud.gif";
+import sqlImage from "../../assets/SQL.gif";
+import osImage from "../../assets/operatingSystem.gif";
+// import cyberSecurityImage from "../../assets/cyberSecurity.gif";
+// import softwareEngineeringImage from "../../assets/software-engineering.gif";
+
 import "./cs.css";
 const PageCs = () => {
   return (
@@ -9,19 +15,31 @@ const PageCs = () => {
       <div>
         <style>
           {`
-    
+      .box-card {
+        background-image: url(${bgImage}) !important;
+      }
 
-    .box-card {
-    background-image: url(${bgImage}) !important;
-    }
-    .box-card-two{
-       background-image: url(${computerNetworkImg}) !important;
-    }
-       .box-card-three{
-       background-image: url(${dbmsImage})} !important;
-       
-          }
-      `}
+      .box-card-two {
+        background-image: url(${computerNetworkImg}) !important;
+      }
+
+      .box-card-three {
+        background-image: url(${dbmsImage}) !important;
+      }
+
+      .box-card-four {
+        background-image: url(${cloudComputingImage}) !important;
+      }
+        .box-card-five{
+        background-image: url(${sqlImage}) !important;}
+
+       .box-card-six{
+        background-image: url({${osImage}) !important;
+        }
+        .box-card-seven{
+        background-image: url({${cloudComputingImage}) !important;
+        }
+    `}
         </style>
       </div>
       <div className="parent-container">
@@ -99,12 +117,12 @@ const PageCs = () => {
         {/* Section-2 covering all cards */}
         <section className="flex justify-around">
           <div className="card">
-            <div className="box-card card-2 w-3 bg-hero-pattern">
+            <div className="box-card-four card-2 w-3 bg-hero-pattern">
               <div className="border-2"></div>
               <div className="content-2">
                 <div className="card-logo-2">
                   <div className="logo2">
-                    <span>DSA</span>
+                    <span>Cloud-Computing</span>
                   </div>
                   <span className="trail-2"></span>
                 </div>
@@ -116,12 +134,12 @@ const PageCs = () => {
           </div>
 
           <div className="card">
-            <div className="box-card card-2 w-3 bg-hero-pattern">
+            <div className="box-card-five card-2 w-3 bg-hero-pattern">
               <div className="border-2"></div>
               <div className="content-2">
                 <div className="card-logo-2">
                   <div className="logo2">
-                    <span>DSA</span>
+                    <span className="text-red-400 text-2xl">SQL-database</span>
                   </div>
                   <span className="trail-2"></span>
                 </div>
@@ -133,12 +151,12 @@ const PageCs = () => {
           </div>
 
           <div className="card">
-            <div className="box-card card-2 w-3 bg-hero-pattern">
+            <div className="box-card-six card-2 w-3 bg-hero-pattern">
               <div className="border-2"></div>
               <div className="content-2">
                 <div className="card-logo-2">
                   <div className="logo2">
-                    <span>DSA</span>
+                    <span>Operating-system</span>
                   </div>
                   <span className="trail-2"></span>
                 </div>
@@ -155,7 +173,7 @@ const PageCs = () => {
 
         <section className="flex justify-around">
           <div className="card">
-            <div className="box-card card-2 w-3 bg-hero-pattern">
+            <div className="box-card-seven card-2 w-3 bg-hero-pattern">
               <div className="border-2"></div>
               <div className="content-2">
                 <div className="card-logo-2">
@@ -172,7 +190,7 @@ const PageCs = () => {
           </div>
 
           <div className="card">
-            <div className="box-card card-2 w-3 bg-hero-pattern">
+            <div className="box-card-eight card-2 w-3 bg-hero-pattern">
               <div className="border-2"></div>
               <div className="content-2">
                 <div className="card-logo-2">
@@ -189,7 +207,7 @@ const PageCs = () => {
           </div>
 
           <div className="card">
-            <div className="box-card card-2 w-3 bg-hero-pattern">
+            <div className="box-card-nine card-2 w-3 bg-hero-pattern">
               <div className="border-2"></div>
               <div className="content-2">
                 <div className="card-logo-2">
@@ -207,11 +225,61 @@ const PageCs = () => {
         </section>
         {/* Section-3 End! */}
 
+        <section className="flex justify-around">
+          <div className="card">
+            <div className="box-card-ten card-2 w-3 bg-hero-pattern">
+              <div className="border-2"></div>
+              <div className="content-2">
+                <div className="card-logo-2">
+                  <div className="logo2">
+                    <span>DSA</span>
+                  </div>
+                  <span className="trail-2"></span>
+                </div>
+                <span className="logo-bottom-text-2">Krish.io</span>
+                {/* Add a unique identifier */}
+              </div>
+              <span className="bottom-text-2">Space.io</span>
+            </div>
+          </div>
 
+          <div className="card">
+            <div className="box-card card-2 w-3 bg-hero-pattern">
+              <div className="border-2"></div>
+              <div className="content-2">
+                <div className="card-logo-2">
+                  <div className="logo2">
+                    <span>DSA</span>
+                  </div>
+                  <span className="trail-2"></span>
+                </div>
+                <span className="logo-bottom-text-2">Krish.io</span>
+                {/* Add a unique identifier */}
+              </div>
+              <span className="bottom-text-2">Space.io</span>
+            </div>
+          </div>
 
-{/* Section-4 Started */}
-{/* Section */}
+          <div className="card">
+            <div className="box-card card-2 w-3 bg-hero-pattern">
+              <div className="border-2"></div>
+              <div className="content-2">
+                <div className="card-logo-2">
+                  <div className="logo2">
+                    <span>DSA</span>
+                  </div>
+                  <span className="trail-2"></span>
+                </div>
+                <span className="logo-bottom-text-2">Krish.io</span>
+                {/* Add a unique identifier */}
+              </div>
+              <span className="bottom-text-2">Space.io</span>
+            </div>
+          </div>
+        </section>
 
+        {/* Section-4 Started */}
+        {/* Section */}
       </div>
       {/* 👆🏻 Main div for whole component! */}
     </>
