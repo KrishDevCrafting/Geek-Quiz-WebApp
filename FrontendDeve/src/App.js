fd085112481fce0fd05ee8ed473a681f2bc7a092
Krish <krishkumer150@gmail.com>
2024-10-09 21:32:15 +0530

    Frontend

--- FrontendDeve/src/App.js
+++ FrontendDeve/src/App.js
@@ -4,9 +4,10 @@ import LoginComponent from "./components/Pages/Auth/Login.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./Home.jsx";
 import { QuizComponent } from "./components/home/QuizPage.jsx";
-import Dropdown from "./components/Test/testing.js";
+
 import FeedBack from "./components/Feedback/FeedBack.jsx";
 import PageCs from "./components/QuizCategories/Computer-Sci.jsx";
+import Testing from "./components/Test/testing.js";
 const App = () => {
   return (
     <>
@@ -17,7 +18,7 @@ const App = () => {
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<LoginComponent />} />
             <Route path="/QuizPage" element={<QuizComponent />} />
-            <Route path="/testing" element={<Dropdown />} />
+            <Route path="/testing" element={<Testing />} />
             <Route path="/value" element={<FeedBack />} />
             <Route path="/computer-science" element={<PageCs />} />
           </Routes>
--- FrontendDeve/src/components/Test/testing.js
+++ FrontendDeve/src/components/Test/testing.js
@@ -1,49 +1,25 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-function Dropdown() {
-  const notify = () =>
-    toast.success("🦄 Wow so easy!", {
-      position: "top-center",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "colored",
-    });
+import React from "react";
+import "./testing.css"
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+
+
+const Testing = () => {
+
+
   return (
     <>
-      {" "}
-      <div className="text-center">
-        <h1>Hello-World!</h1>
-        <form onSubmit={handleSubmit}>
-          <label className="px-4 text-2xl">Name</label>
-          <input type="text" placeholder="name" />
-          <br />
-          <label className="px-4 text-2xl">Email</label>
 
-          <input type="email" placeholder="Enter Email id!" />
-          <br />
-          <label className="px-4 text-2xl">Password</label>
 
-          <input type="password" placeholder="Password" />
-          <br />
-          <button
-            className="bg-blue-600 px-4 m-8 hover:bg-blue-800 text-white"
-            onClick={notify}>
-            Submit
-          </button>
-          <ToastContainer />
-        </form>
+
+
+
+
+
+      <div>
+        <h1>Hello</h1>
       </div>
     </>
   );
-}
+};
 
-export default Dropdown;
+export default Testing;
