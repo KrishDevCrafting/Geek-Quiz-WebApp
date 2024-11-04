5de0216d1d16806f43b670347bfec755e160e353
Krish <krishkumer150@gmail.com>
2024-11-04 23:28:55 +0530

    up

--- FrontendDeve/src/LeaderBoard/Page.jsx
+++ FrontendDeve/src/LeaderBoard/Page.jsx
@@ -2,12 +2,7 @@ const LeaderPageComponent = () => {
   return (
     <>
       <div>
-        <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde
-          reiciendis quae veniam repellat ullam, aut repudiandae a facere
-          quisquam, explicabo dolor fuga corrupti, mollitia cum ex at? Nisi,
-          voluptatem reprehenderit?
-        </h1>
+       
       </div>
     </>
   );
