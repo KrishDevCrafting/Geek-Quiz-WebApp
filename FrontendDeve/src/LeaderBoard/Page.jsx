65278a4fbac36ad9dbc1a23204c0b849339f6884
Krish <krishkumer150@gmail.com>
2024-11-07 14:28:21 +0530

    updated

--- FrontendDeve/src/LeaderBoard/Page.jsx
+++ FrontendDeve/src/LeaderBoard/Page.jsx
@@ -1,8 +1,26 @@
+import React from "react";
+import "./page.css";
+
 const LeaderPageComponent = () => {
   return (
     <>
-      <div>
-       
+      <div className="leftSide-nav">
+        <div className="leftside-navBox">
+          <ol>
+            <li>GeekQuiz</li>
+            <li>
+              <input type="text" placeholder="search" />
+            </li>
+          </ol>
+        </div>
+        <div className="leftside-navBox">
+          <ul>
+            <li>Home</li>
+            <li>FeedBack</li>
+            <li>Update</li>
+            <li>Browrse-more</li>
+          </ul>
+        </div>
       </div>
     </>
   );
