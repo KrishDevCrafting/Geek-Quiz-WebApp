d30071973404d1c1470d59401f2f6fae958a8cf7
KrishDevCrafting <krishkumer150@gmail.com>
2024-11-26 20:47:19 +0530

    LeaderBoard-Updated

--- FrontendDeve/src/LeaderBoard/Page.jsx
+++ FrontendDeve/src/LeaderBoard/Page.jsx
@@ -5,21 +5,26 @@ const LeaderPageComponent = () => {
   return (
     <>
       <div className="leftSide-nav">
-        <div className="leftside-navBox">
-          <ol>
-            <li>GeekQuiz</li>
-            <li>
-              <input type="text" placeholder="search" />
-            </li>
-          </ol>
+        <div className="sideNavbar-div">
+          <div>
+            <ol>
+              <li>GeekQuiz</li>
+              <li>
+                <input type="text" placeholder="search" />
+              </li>
+            </ol>
+
+            <ul>
+              <li>Home</li>
+              <li>FeedBack</li>
+              <li>Update</li>
+              <li>Browrse-more</li>
+            </ul>
+          </div>
         </div>
-        <div className="leftside-navBox">
-          <ul>
-            <li>Home</li>
-            <li>FeedBack</li>
-            <li>Update</li>
-            <li>Browrse-more</li>
-          </ul>
+        <div className="heading-div">
+          <h1 className="text-red-900">See where You are!</h1>
+          
         </div>
       </div>
     </>
